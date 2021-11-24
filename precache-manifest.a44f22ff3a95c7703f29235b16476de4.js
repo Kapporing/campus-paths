78e9426339d6a580f1a913211af1ca18d7429506
Ronald Widjaja <ronaldwilliamwidjaja@gmail.com>
2021-11-24 12:18:23 -0800

    Updates

---
+++ precache-manifest.a44f22ff3a95c7703f29235b16476de4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46cc523b31d7740d0ebfef830b137f0d",
+    "url": "/campus-paths/index.html"
+  },
+  {
+    "revision": "38c17cef5d46381c200b",
+    "url": "/campus-paths/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c1c38e3a9074ea21d43a",
+    "url": "/campus-paths/static/css/main.9f9e8488.chunk.css"
+  },
+  {
+    "revision": "38c17cef5d46381c200b",
+    "url": "/campus-paths/static/js/2.497c1883.chunk.js"
+  },
+  {
+    "revision": "8405fc7db03aa55859ceaaa8183fa901",
+    "url": "/campus-paths/static/js/2.497c1883.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c38e3a9074ea21d43a",
+    "url": "/campus-paths/static/js/main.c1d30f2d.chunk.js"
+  },
+  {
+    "revision": "53dffd1317c145af2a9c",
+    "url": "/campus-paths/static/js/runtime-main.992fff7e.js"
+  }
+]);
